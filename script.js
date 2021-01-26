cc0582b832c20fcde92bb8b0fff03b68b753cef9
Vadym <hulchenko@shaw.ca>
2021-01-25 21:36:06 -0700

    modify script.js, update css/html, add pictures to the project

--- script.js
+++ script.js
@@ -1,55 +1,74 @@
 let gameWeapon = ["Rock", "Paper", "Scissors"];
-        let playerSelection;
-        let computerSelection = computerPlay();
+        const player = 0;
+        const computer = 0;
+        const playerScore = document.querySelector("#player-score");
+        const computerScore = document.querySelector("#computer-score");
+        const scoreBoard = document.querySelector(".score-board");
+        const result = document.querySelector(".result");
+        const rock = document.getElementById("rock");
+        const paper = document.getElementById("paper");
+        const scissors = document.getElementById("scissors");
 
-        userInput();
 
-        //first function:
-        function userInput () {            
-            let playerSelection = window.prompt("Make your choice: Rock, Paper or Scissors?", "");
-                 if ((playerSelection.toLowerCase()=="rock")
-                 || (playerSelection.toLowerCase()=="paper")
-                 || (playerSelection.toLowerCase()=="scissors")) {
-            console.log(gameRound(playerSelection, computerSelection))
-            } else {
-                        alert ("Wrong input!");
-                        userInput();
-            }
-        }
+    function game(userChoice){
+        console.log(userChoice);
+    }
+   
 
-        //second function:
-        function computerPlay() {
-            return gameWeapon [Math.floor(Math.random()*gameWeapon.length)];
-        }
-        // to test: console.log(computerPlay());
-
-        //third function:
-        function gameRound(playerSelection, computerSelection) {
-            playerSelection;
-            computerSelection = computerPlay().toLowerCase();
-            
-                if (playerSelection === computerSelection) {
-                return ("Draw!");
-                    }
-                    if ((playerSelection == "rock" && computerSelection == "paper")
-                    ||  (playerSelection == "paper" && computerSelection == "scissors") 
-                    ||  (playerSelection == "scissors" && computerSelection == "rock")) {
-                    return ("Computer won!");
-                     }
-                        else {
-                         return ("You won!");
-                        }
-        }
-
-        //let fiveRounds = [
-        //    console.log(gameRound(playerSelection, computerSelection)),
-        //    console.log(gameRound(playerSelection, computerSelection)),
-        //    console.log(gameRound(playerSelection, computerSelection)),
-        //    console.log(gameRound(playerSelection, computerSelection)),
-        //    console.log(gameRound(playerSelection, computerSelection)),
-        //]
-       
-        //fourth function:
-        //function game() {
-        //    return fiveRounds;
-        //}
+    function main() {
+        rock.addEventListener("click", function(){
+            game("1");
+        })
+        paper.addEventListener("click", function(){
+            game("2");
+        })
+        scissors.addEventListener("click", function(){
+            game("3");
+        })
+        scoreBoard.addEventListener("click", function(){
+            game("Score!");
+        })
+    }
+    main();
+    
+//        let playerSelection;
+//        let computerSelection = computerPlay();
+//
+//        userInput();
+//
+//        //first function:
+//        function userInput () {            
+//            let playerSelection = document.querySelector//(button);
+//                 if ((playerSelection.toLowerCase()//=="rock")
+//                 || (playerSelection.toLowerCase()//=="paper")
+//                 || (playerSelection.toLowerCase()//=="scissors")) {
+//            console.log(gameRound(playerSelection, //computerSelection))
+//            } else {
+//                        alert ("Wrong input!");
+//                        userInput();
+//            }
+//        }
+//
+//        //second function:
+//        function computerPlay() {
+//            return gameWeapon [Math.floor(Math.random()//*gameWeapon.length)];
+//        }
+//        // to test: console.log(computerPlay());
+//
+//        //third function:
+//        function gameRound(playerSelection, //computerSelection) {
+//            playerSelection;
+//            computerSelection = computerPlay().toLowerCase//();
+//            
+//                if (playerSelection === //computerSelection) {
+//                return ("Draw!");
+//                    }
+//                    if ((playerSelection == "rock" && //computerSelection == "paper")
+//                    ||  (playerSelection == "paper" && //computerSelection == "scissors") 
+//                    ||  (playerSelection == "scissors" && //computerSelection == "rock")) {
+//                    return ("Computer won!");
+//                     }
+//                        else {
+//                         return ("You won!");
+//                        }
+//        }
